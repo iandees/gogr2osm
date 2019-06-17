aba944c1e3113b691402b8054978ccfc2837a6aa
Ian Dees <ian.dees@gmail.com>
2019-06-17 10:19:31 -0500

    Add starlark for tag processing

--- cmd/gogr2osm.go
+++ cmd/gogr2osm.go
@@ -9,6 +9,7 @@ import (
 
 	gdal "github.com/lukeroth/gdal"
 	osm "github.com/paulmach/osm"
+	starlark "go.starlark.net/starlark"
 )
 
 func nodeHash(lon, lat float64) string {
@@ -91,16 +92,17 @@ func (n *osmGenerator) GetOSM() *osm.OSM {
 	}
 }
 
-func (n *osmGenerator) TranslateLinestring(points [][2]float64) {
+func (n *osmGenerator) TranslateLinestring(points [][2]float64) *osm.Way {
 	way := n.GetNextWay()
 	for _, point := range points {
 		node := n.TranslatePoint(point[0], point[1])
 		way.Nodes = append(way.Nodes, osm.WayNode{ID: node.ID})
 	}
 	n.ways = append(n.ways, way)
+	return way
 }
 
-func (n *osmGenerator) TranslatePolygon(rings [][][2]float64) {
+func (n *osmGenerator) TranslatePolygon(rings [][][2]float64) osm.Element {
 	ways := osm.Ways{}
 	for _, ring := range rings {
 		way := n.GetNextWay()
@@ -114,23 +116,25 @@ func (n *osmGenerator) TranslatePolygon(rings [][][2]float64) {
 	if len(ways) > 1 {
 		// If this is a polygon with holes, convert to a multipolygon relation
 		multipolygonRelation := n.GetNextRelation()
-		multipolygonRelation.Tags = append(multipolygonRelation.Tags, osm.Tag{Key: "type", Value: "multipolygon"})
+		multipolygonRelation.Tags = osm.Tags{osm.Tag{Key: "type", Value: "multipolygon"}}
 		multipolygonRelation.Members = append(multipolygonRelation.Members, osm.Member{
-			Type: ways[0].ObjectID().Type(),
-			Ref:  ways[0].ObjectID().Ref(),
+			Type: osm.TypeWay,
+			Ref:  int64(ways[0].ID),
 			Role: "outer",
 		})
 
 		for i := 1; i < len(ways); i++ {
 			multipolygonRelation.Members = append(multipolygonRelation.Members, osm.Member{
-				Type: ways[i].ObjectID().Type(),
-				Ref:  ways[i].ObjectID().Ref(),
+				Type: osm.TypeWay,
+				Ref:  int64(ways[i].ID),
 				Role: "inner",
 			})
 		}
 		n.relations = append(n.relations, multipolygonRelation)
+		return multipolygonRelation
 	} else {
 		n.ways = append(n.ways, ways[0])
+		return ways[0]
 	}
 }
 
@@ -153,19 +157,99 @@ func extractPoints(geometry gdal.Geometry) [][2]float64 {
 	return points
 }
 
-func main() {
-	flag.Parse()
-	filename := flag.Arg(0)
-	if filename == "" {
-		log.Fatalf("No filename")
+type starlarkMapper struct {
+	thread         *starlark.Thread
+	filterTagsFunc *starlark.Function
+}
+
+func NewStarlarkMapper(filename string) (*starlarkMapper, error) {
+	thread := &starlark.Thread{Name: "osm mapper"}
+	globals, err := starlark.ExecFile(thread, filename, nil, nil)
+	if err != nil {
+		return nil, err
 	}
 
-	log.Printf("Found %d drivers", gdal.GetDriverCount())
+	filterTagsFn, found := globals["filter_tags"]
+	if !found {
+		log.Printf("filter_tags function not found")
+	}
 
-	source := gdal.OpenDataSource(filename, 0)
+	return &starlarkMapper{
+		thread:         thread,
+		filterTagsFunc: filterTagsFn.(*starlark.Function),
+	}, nil
+}
+
+func (m *starlarkMapper) FilterTags(schema gdal.FeatureDefinition, feature *gdal.Feature) (osm.Tags, error) {
+	starlarkProps := starlark.NewDict(schema.FieldCount())
+	for i := 0; i < schema.FieldCount(); i++ {
+		fieldDefinition := schema.FieldDefinition(i)
+		name := fieldDefinition.Name()
+		var val starlark.Value
+		switch fieldDefinition.Type() {
+		case gdal.FT_Integer:
+			val = starlark.MakeInt(feature.FieldAsInteger(i))
+		case gdal.FT_String:
+			val = starlark.String(feature.FieldAsString(i))
+		case gdal.FT_Real:
+			val = starlark.Float(feature.FieldAsFloat64(i))
+		case gdal.FT_Integer64:
+			val = starlark.MakeInt64(feature.FieldAsInteger64(i))
+		default:
+			return nil, fmt.Errorf("couldn't map gdal field %s (type %d)", name, fieldDefinition.Type())
+		}
+		starlarkProps.SetKey(starlark.String(name), val)
+	}
+
+	v, err := starlark.Call(m.thread, m.filterTagsFunc, starlark.Tuple{starlarkProps}, nil)
+	if err != nil {
+		if evalErr, ok := err.(*starlark.EvalError); ok {
+			log.Fatal(evalErr.Backtrace())
+		}
+		log.Fatalf("Couldn't run filter_tag: %+v", err)
+	}
+
+	if v.Type() != "dict" {
+		log.Fatalf("Expected a dict from filter_tag and got %s", v.Type())
+	}
+
+	tagsDict := v.(*starlark.Dict)
+	tags := make(osm.Tags, tagsDict.Len())
+
+	for i, t := range tagsDict.Items() {
+		if t[0].Type() != "string" && t[1].Type() != "string" {
+			return nil, fmt.Errorf("filter_tag returned dicts with non-string keys or values")
+		}
+
+		k := t[0].(starlark.String)
+		v, ok := t[1].(starlark.String)
+		if !ok {
+			continue
+		}
+
+		tags[i] = osm.Tag{Key: k.GoString(), Value: v.GoString()}
+	}
+
+	return tags, nil
+}
+
+func main() {
+	inputFilename := flag.String("input", "", "input path for OGR-supported file")
+	outputFilename := flag.String("output", "", "output path for OSM XML data")
+	mapperFilename := flag.String("mapper", "", "name of starlark file to perform mapping of input geometry to output OSM XML")
+	flag.Parse()
+
+	if *inputFilename == "" {
+		log.Fatalf("No input filename specified")
+	}
+
+	source := gdal.OpenDataSource(*inputFilename, 0)
 	defer source.Destroy()
 
-	log.Printf("Layer count: %d", source.LayerCount())
+	if source.LayerCount() != 1 {
+		log.Fatalf("Input has more than one layer. We only support a single layer for now.")
+	}
+
 	log.Printf("Driver: %s", source.Driver().Name())
 
 	layer := source.LayerByIndex(0)
@@ -174,6 +258,19 @@ func main() {
 		log.Fatalf("Couldn't get feature count")
 	}
 	log.Printf("Layer name: %s; %d features", layer.Name(), featureCount)
+
+	if *outputFilename == "" {
+		log.Fatalf("No output filename specified")
+	}
+
+	if *mapperFilename == "" {
+		log.Fatalf("No mapper filename specified")
+	}
+
+	mapper, err := NewStarlarkMapper(*mapperFilename)
+	if err != nil {
+		log.Fatalf("Couldn't create mapper: %+v", err)
+	}
 
 	schema := layer.Definition()
 	fieldCount := schema.FieldCount()
@@ -209,7 +306,11 @@ func main() {
 				log.Fatalf("Don't know how to handle point type with more than one point")
 			}
 
-			osmGenerator.TranslatePoint(points[0][0], points[0][1])
+			osmPrimitive := osmGenerator.TranslatePoint(points[0][0], points[0][1])
+			osmPrimitive.Tags, err = mapper.FilterTags(schema, nextFeature)
+			if err != nil {
+				log.Fatalf("Couldn't map tags: %+v", err)
+			}
 
 		case gdal.GT_LineString,
 			gdal.GT_LineString25D,
@@ -217,7 +318,11 @@ func main() {
 			gdal.GT_MultiLineString25D:
 
 			points := extractPoints(geometry)
-			osmGenerator.TranslateLinestring(points)
+			osmPrimitive := osmGenerator.TranslateLinestring(points)
+			osmPrimitive.Tags, err = mapper.FilterTags(schema, nextFeature)
+			if err != nil {
+				log.Fatalf("Couldn't map tags: %+v", err)
+			}
 
 		case gdal.GT_Polygon,
 			gdal.GT_Polygon25D,
@@ -225,7 +330,16 @@ func main() {
 			gdal.GT_MultiPolygon25D:
 
 			rings := extractRings(geometry)
-			osmGenerator.TranslatePolygon(rings)
+			osmPrimitive := osmGenerator.TranslatePolygon(rings)
+			switch osmPrimitive.(type) {
+			case *osm.Way:
+				osmPrimitive.(*osm.Way).Tags, err = mapper.FilterTags(schema, nextFeature)
+			case *osm.Relation:
+				osmPrimitive.(*osm.Relation).Tags, err = mapper.FilterTags(schema, nextFeature)
+			}
+			if err != nil {
+				log.Fatalf("Couldn't map tags: %+v", err)
+			}
 
 		default:
 			log.Fatalf("unknown geometry type %d", geometry.Type())
@@ -235,12 +349,12 @@ func main() {
 	}
 
 	osmOutput := osmGenerator.GetOSM()
-	data, err := xml.Marshal(osmOutput)
+	data, err := xml.MarshalIndent(osmOutput, "", "  ")
 	if err != nil {
 		log.Fatalf("Couldn't marshall OSM output: %+v", err)
 	}
 
-	output, err := os.Create("output.osm")
+	output, err := os.Create(*outputFilename)
 	if err != nil {
 		log.Fatalf("Couldn't create output file: %+v", err)
 	}
